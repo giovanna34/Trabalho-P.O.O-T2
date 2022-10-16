0add73ac74d36e46a07fa45b29502d43153dab26
Sophia Mello <113708065+SophiaMel@users.noreply.github.com>
2022-10-16 20:44:53 -0300

    Add files via upload

---
+++ Questão10/src/tabuada/Principal.java
@@ -0,0 +1,19 @@
+package tabuada;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("[Tabuada de Mulplicação] ");
+		for (int i = 1; i<=10; i++){
+			for (int valor = 1; valor<=10; valor++){
+				System.out.println (i+" x "+ valor + " = " + i*valor);
+				}
+			System.out.println("\n"); }
+		System.out.println("APROVEITE A TABUADA, USE SEMPRE QUE PRECISAR  (≧◡≦)");
+		}
+
+
+			
+
+	}
+
